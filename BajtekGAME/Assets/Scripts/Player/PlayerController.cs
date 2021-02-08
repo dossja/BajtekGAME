930cc051bd58e48ace887aa51aa1ed235162b8d8
Dossja <domka_24@interia.pl>
2021-02-08 16:52:32 +0100

    PlayerMovement and Collecting

---
+++ BajtekGAME/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidbody2D;
+
+    public TextMeshProUGUI collectedText;
+    public static int collectedAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+
+        collectedText.text = "Items Collected: " + collectedAmount;
+    }
+}
