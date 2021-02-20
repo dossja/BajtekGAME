09a22464393a099e9feca2591ea8838c9e432602
Dossja <domka_24@interia.pl>
2021-02-20 10:31:38 +0100

    Shooting & started level generation

--- BajtekGAME/Assets/Scripts/Player/PlayerController.cs
+++ BajtekGAME/Assets/Scripts/Player/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : MonoBehaviour
     public TextMeshProUGUI collectedText;
     public static int collectedAmount;
 
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float fireDelay;
+    private float lastFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +26,28 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        float shootHorizontal = Input.GetAxis("ShootHorizontal");
+        float shootVertical = Input.GetAxis("ShootVertical");
+
+        if((shootHorizontal != 0 || shootVertical !=0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHorizontal, shootVertical);
+            lastFire = Time.time;
+        }
+
         rigidbody2D.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
 
         collectedText.text = "Items Collected: " + collectedAmount;
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
+           );
     }
 }
