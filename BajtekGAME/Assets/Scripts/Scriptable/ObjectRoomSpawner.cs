193716b306ec76508f27610f8021943dcdaa1c3c
Dossja <domka_24@interia.pl>
2021-04-14 10:57:31 +0200

    Enemy spawner part 1

---
+++ BajtekGAME/Assets/Scripts/Scriptable/ObjectRoomSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRoomSpawner : MonoBehaviour
+{
+    public struct RandomSpawner
+    {
+        public string name;
+        public SpawnerData spawnerData;
+    }
+
+    public GridController grid;
+    public RandomSpawner[] spawnerData;
+
+    private void Start()
+    {
+        
+    }
+}
